269f27114329c873be6fa40e633cccba0f854af2
RoselleB <Roselleblanco@hotmail.com>
2026-04-20 15:25:48 -0600

    Update CustomerManager

--- FinalPrototype/src/Manager/CustomerManager.java
+++ FinalPrototype/src/Manager/CustomerManager.java
@@ -1,63 +1,74 @@
-package villageRentals;
+package manager;
 
 import model.Customer;
 import java.io.*;
-import java.util.List;
-import java.util.Scanner;
-
+ 
 public class CustomerManager {
-
-    private final String FILE_NAME = "customers.txt";
-
-    // ✅ ADD CUSTOMER (saves to file)
-    public void addCustomer(Customer customer) {
+ 
+    private static final String FILE_NAME = "customers.txt";
+ 
+    public static void addCustomer(Customer customer) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME, true))) {
-
+ 
             writer.write(customer.getCustomerId() + "," +
                          customer.getFirstName() + "," +
                          customer.getLastName() + "," +
                          customer.getPhone() + "," +
                          customer.getEmail() + "," +
                          customer.isBanned());
-
+ 
             writer.newLine();
-
+ 
         } catch (IOException e) {
-            System.out.println("Error saving customer.");
+            System.out.println("Error writing to file.");
         }
     }
-
-    public void getAllCustomers() {
+ 
+    public static void getAllCustomers() {
         try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
-
+ 
             String line;
-
             while ((line = reader.readLine()) != null) {
-
-                Scanner lineScanner = new Scanner(line);
-                lineScanner.useDelimiter(",");
-
-                int id = lineScanner.nextInt();
-                String firstName = lineScanner.next();
-                String lastName = lineScanner.next();
-                String phone = lineScanner.next();
-                String email = lineScanner.next();
-                boolean isBanned = lineScanner.nextBoolean();
-
-                Customer c = new Customer(id, firstName, lastName, phone, email, isBanned);
+ 
+                String[] data = line.split(",");
+ 
+                Customer c = new Customer(
+                        Integer.parseInt(data[0]),
+                        data[1],
+                        data[2],
+                        data[3],
+                        data[4],
+                        Boolean.parseBoolean(data[5])
+                );
+ 
                 System.out.println(c);
-
-                lineScanner.close();
             }
-
+ 
         } catch (IOException e) {
             System.out.println("No customers found.");
         }
     }
-
-    
+ 
+    public static boolean idExists(int idToCheck) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+ 
+            String line;
+            while ((line = reader.readLine()) != null) {
+ 
+                String[] data = line.split(",");
+                int id = Integer.parseInt(data[0]);
+ 
+                if (id == idToCheck) {
+                    return true;
+                }
+            }
+ 
+        } catch (IOException e) {
+            return false; // file doesn't exist yet
+        }
+ 
+        return false;
+    }
 }
-
-
 
 
